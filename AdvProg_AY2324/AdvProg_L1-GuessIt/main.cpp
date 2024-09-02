944f5bd5336c3f63abe7c4466263d6afc3d378c4
khoapd0114 <pdokhoa1411@gmail.com>
2024-09-02 09:45:31 +0700

    ?

--- AdvProg_AY2324/AdvProg_L1-GuessIt/main.cpp
+++ AdvProg_AY2324/AdvProg_L1-GuessIt/main.cpp
@@ -1,19 +1,10 @@
 #include <iostream>
-// write code to calcute 2 numbers 
-int main()
+#include <string>
+#include <cmath>
+#include "guessit.h"
+
+
+int main(int argc, char* argv[])
 {
-    int num1 = 10;
-    int num2 = 5;
-
-    int sum = num1 + num2;
-    int difference = num1 - num2;
-    int product = num1 * num2;
-    float quotient = static_cast<float>(num1) / num2;
-    int remainder = num1 % num2;
-    
-    std::cout << "Sum: " << sum << std::endl;
-    std::cout << "Difference: " << difference << std::endl;
-    std::cout << "Product: " << product << std::endl;
-    std::cout << "Quotient: " << quotient << std::endl;
-
+    run();
 }
